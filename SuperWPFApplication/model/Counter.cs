d8adc959be93a237e588352792b283f91e1868d0
Vinichenko Vladimir <vovanvinni@gmail.com>
2020-11-12 17:31:09 +0300

    Добавьте файлы проекта.

---
+++ SuperWPFApplication/model/Counter.cs
@@ -0,0 +1,100 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace SuperWPFApplication.model
+{
+    public class Counter: INotifyPropertyChanged
+    {
+        private int number;
+        private int a = 0;
+        private int b = 0;
+        private int c = 0;
+        private int? d = null;
+        private double? x1 = null;
+        private double? x2 = null;
+
+
+        public int number_arg
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                OnPropertyChanged("num_argument");
+            }
+        }
+        public int a_argument
+        {
+            get { return a; }
+            set 
+            { 
+                a = value;
+                OnPropertyChanged("a_argument");
+            }
+        }
+
+        public int b_argument
+        {
+            get { return b; }
+            set 
+            {
+                OnPropertyChanged("b_argument");
+                b = value; 
+            }
+        }
+
+        public int c_argument
+        {
+            get { return c; }
+            set 
+            {
+                OnPropertyChanged("c_argument"); 
+                c = value; 
+            }
+        }
+
+        public int? d_result
+        {
+            get { return d; }
+        }
+
+        public double? x1_result
+        {
+            get { return x1; }
+        }
+
+        public double? x2_result
+        {
+            get { return x2; }
+        }
+
+        public void calculate()
+        {
+                d = b * b - 4 * a * c;
+                OnPropertyChanged("d_result");
+                if (d < 0)
+                    return;
+                if (d == 0)
+                {
+                    x1 = -b / (2 * a);
+                    OnPropertyChanged("x1_result");
+                    x2 = null;
+                    OnPropertyChanged("x2_result");
+                    return;
+                }
+                x1 = (-b + Math.Sqrt(Convert.ToDouble(d))) / (2 * a);
+                OnPropertyChanged("x1_result");
+                x2 = (-b - Math.Sqrt(Convert.ToDouble(d))) / (2 * a);
+                OnPropertyChanged("x2_result");
+
+                return; 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ SuperWPFApplication/view_model/vmCounter.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System;
+
+namespace SuperWPFApplication.view_model
+{
+    public class vmCounter : INotifyPropertyChanged
+    {
+        private model.Counter selected_counter;
+        private RelayCommand addCommand;
+        public ObservableCollection<model.Counter> Counters { get; set; }
+        public model.Counter SelectedCounter
+        {
+            get { return selected_counter; }
+            set
+            {
+                selected_counter = value;
+                OnPropertyChanged("SelectedCounter");
+            }
+        }
+        
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                Console.WriteLine("Button Pressed");
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      selected_counter.calculate();
+                  }));
+            }
+        }
+
+        public vmCounter()
+        {
+            Counters = new ObservableCollection<model.Counter>
+            {
+                new model.Counter { number_arg = 0, a_argument = 0, b_argument = 0, c_argument = 0 },
+                new model.Counter { number_arg = 1, a_argument = 1, b_argument = 1, c_argument = 1 },
+                new model.Counter { number_arg = 2, a_argument = -5, b_argument = 2, c_argument = 4 },
+                new model.Counter { number_arg = 3, a_argument = 8, b_argument = -20, c_argument = 1 },
+            };
+            SelectedCounter = Counters[1];
+
+            //selected_counter = new model.Counter { a_argument = 0, b_argument = 0, c_argument = 0 };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
